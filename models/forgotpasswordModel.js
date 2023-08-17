baec096da3b3d5b61ebeabcec72c177f0bd797cd
Amit Adhikari <amitadhikari525252@gmail.com>
2023-08-17 17:31:56 +0530

    Sending Reset Password Link

--- models/forgotpasswordModel.js
+++ models/forgotpasswordModel.js
@@ -1,13 +1,14 @@
 const Sequelize = require('sequelize');
 const sequelize =require('../util/db')
 
-const ForgotPassword = sequelize.define('forgotpassword_tb', {
+const ForgotPassword = sequelize.define('forgotpassword', {
     id:{
         type: Sequelize.UUID,
         allowNull: false,
         primaryKey: true
     },
     active: Sequelize.BOOLEAN,  
+    expiresby: Sequelize.DATE
 });
 
 module.exports = ForgotPassword;
