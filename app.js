59f3a70efa3741df7ad16dc4ecd339188c0b0801
Amit Adhikari <amitadhikari525252@gmail.com>
2023-08-17 17:31:56 +0530

    Sending Reset Password Link

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const cors = require('cors');
 const sequelize = require('./util/db');
 const dotenv = require('dotenv');
 
-// const helmet =require('helmet');
-// const morgan = require('morgan');
 
 const User = require('./models/userModel');
 const Expense = require('./models/expenseModel');
@@ -17,7 +15,7 @@ const userRoutes = require('./routes/userRoute');
 const expenseRoutes = require('./routes/expenseRoute')
 const purchaseRoutes = require('./routes/purchaseRoute')
 const premiumFeatureRoutes = require('./routes/premiumRoute')
-const forgetPasswordRoutes = require('./routes/forgetPasswordRoute');
+const resetPasswordRoutes = require('./routes/passwordRoute');
 const app = express();
 
 
@@ -27,35 +25,37 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-// app.use(helmet());
-// app.use(morgan('combined',{stream:accessLogstream}))
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/user', userRoutes);
 app.use('/expenses', expenseRoutes);
 app.use('/purchase', purchaseRoutes);
 app.use('/premium', premiumFeatureRoutes);
-app.use('/password',forgetPasswordRoutes);
+app.use('/password',resetPasswordRoutes);
 
 
-//public\Login\login.html
+// Serve main dashboard page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
 });
 
+// Serve main dashboard page
 app.get('/login',(req,res)=>{
   res.sendFile(path.join(__dirname,'public/login','login.html'))
 });
 
+
+// Serve signup page
 app.get('/signup',(req,res)=>{
   res.sendFile(path.join(__dirname,'public/Signup','signup.html'))
 });
 
-
+// Serve expenses page
 app.get('/expenses', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/Home', 'expense.html'));
 });
 
+
+// Define associations
 User.hasMany(Expense);
 Expense.belongsTo(User);
 
@@ -65,7 +65,9 @@ Order.belongsTo(User);
 User.hasMany(Forgotpassword);
 Forgotpassword.belongsTo(User);
 
-sequelize.sync({ alter: true })
+
+// Start server after syncing with database
+sequelize.sync()
   .then(() => {
     app.listen(3000, () => {
       console.log('Server is running on port 3000');
